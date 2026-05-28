601f775788986aa932f5101c03ce6ae432d77929
Crislaine77 <crislaine.santos.barbosa@escola.pr.gov.br>
2026-05-28 19:36:13 -0300

    cronometros

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const textos = document.querySelectorAll(".aba-conteudo");
 
 for (let i = 0; i < botoes.length; i++) {
     botoes[i].onclick = function () {
-        for(let j = 0; j < botoes.length; j++) {
+        for (let j = 0; j < botoes.length; j++) {
             botoes[j].classList.remove("ativo");
             textos[j].classList.remove("ativo");
         }
@@ -13,9 +13,12 @@ for (let i = 0; i < botoes.length; i++) {
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2026-09-05T00:00:00");
+const tempoObjetivo1 = new Date("2026-06-30T00:00:00");
+const tempoObjetivo2 = new Date("2026-08-31T00:00:00");
+const tempoObjetivo3 = new Date("2026-07-31T00:00:00");
+const tempoObjetivo4 = new Date("2026-12-11T00:00:00");
 
-contadores[0].textContent = calculaTempo(tempoObjetivo1)
+const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
 
 function calculaTempo(tempoObjetivo) {
     let tempoAtual = new Date();
@@ -23,11 +26,29 @@ function calculaTempo(tempoObjetivo) {
     let segundos = Math.floor(tempoFinal / 1000);
     let minutos = Math.floor(segundos / 60);
     let horas = Math.floor(minutos / 60);
-    let dias = Math.floor(horas /24);
+    let dias = Math.floor(horas / 24);
 
     segundos %= 60;
     minutos %= 60;
     horas %= 24;
 
-    return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos.";
+    if (tempoFinal > 0) {
+        return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos.";
+    }
+    else {
+        return "Prazo finalizado.";
+    }
 }
+
+function atualizarCronometro () {
+    for (let i=0; i < contadores.length, i++) {
+        contadores[i].textContent = calculaTempo(tempo[i]);
+    }
+}
+
+function comecaCronometro () {
+    atualizarCronometro();
+    setInterval(atualizarCronometro,1000);
+}
+
+comecaCronometro();
